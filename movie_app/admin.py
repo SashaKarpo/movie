f38db1e11c5e1e82f3ce971699cf9bed53ce8722
SashaKarpo <prmzwe@gmail.com>
2022-12-15 17:37:39 +0300

    added method movie_status in admin.py

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -5,7 +5,18 @@ from .models import Movie
 # Register your models here.
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('name', 'rating', 'year', 'budget')
+    list_display = ('name', 'rating', 'year', 'budget', 'movie_status')
     list_editable = ('rating', 'year', 'budget')
     ordering = ['-rating', 'name']
-    list_per_page = 5
+    list_per_page = 10
+
+    @admin.display(ordering='rating', description='Status')
+    def movie_status(self, mov: Movie):
+        if mov.rating < 50:
+            return 'Не стоит смотреть'
+        if mov.rating < 70:
+            return 'На один раз'
+        if mov.rating <=85:
+            return 'Достойный вашего внимания'
+        else:
+            return 'Один из лучших'
