87a2c795178c3bbd5e4a93432fbf81a24b634cd2
SashaKarpo <prmzwe@gmail.com>
2022-12-16 20:01:55 +0300

    element shape customization

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -4,14 +4,43 @@ from django.db.models import QuerySet
 
 
 # Register your models here.
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rating'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', 'Низкий'),
+            ('от 40 до 59', 'Cредний'),
+            ('от 60 до 79', 'Высокий'),
+            ('>=80', 'Высочайший')
+        ]
+
+    def queryset(self, request, queryset: QuerySet):
+        if self.value() == '<40':
+            return queryset.filter(rating__lt=40)
+        if self.value() == 'от 40 до 59':
+            return queryset.filter(rating__gte=40).filter(rating__lt=60)
+        if self.value() == 'от 60 до 79':
+            return queryset.filter(rating__gte=60).filter(rating__lt=80)
+        if self.value() == '>=80':
+            return queryset.filter(rating__gte=80)
+        return queryset
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
+    # fields = []
+    # exclude = ['slug']
+    # readonly_fields = []
+    prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'rating', 'currency', 'budget', 'movie_status')
     list_editable = ('rating', 'currency', 'budget')
     ordering = ['-rating', 'name']
     list_per_page = 10
     actions = ['set_dollar', 'set_euro']
     search_fields = ['name__istartswith', 'rating']
+    list_filter = ['name', 'currency', RatingFilter]
 
     @admin.display(ordering='rating', description='Status')
     def movie_status(self, mov: Movie):
--- movie_app/models.py
+++ movie_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -17,9 +18,9 @@ class Movie(models.Model):
     ]
 
     name = models.CharField(max_length=40)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(100)])
     year = models.IntegerField(null=True, blank=True)
-    budget = models.IntegerField(default=1000000)
+    budget = models.IntegerField(default=1000000, blank=True, validators=[MinValueValidator(1)])
     currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=RUB)
     slug = models.SlugField(default='', null=False, db_index=True)
 
